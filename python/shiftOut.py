e354e20d75abb5fd3054d32f26a74ca831ba98f7
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-24 20:02:46 +1000

    asdf

--- python/shiftOut.py
+++ python/shiftOut.py
@@ -15,11 +15,14 @@ def shiftout(byte):
     GPIO.output(PIN_CLOCK, 1)
     GPIO.output(PIN_CLOCK, 0)
   GPIO.output(PIN_LATCH, 1)
+  time.sleep(1)
 
 
 for x in range(255):
   shiftout(x)
   time.sleep(0.001)
+  
+  
 
 
 GPIO.cleanup()
