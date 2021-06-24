94e44b99be6782f0a1e8108eaf35b8686d5fcdd9
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-24 19:53:38 +1000

    asdf

--- python/shiftOut.py
+++ python/shiftOut.py
@@ -4,6 +4,11 @@ import RPi.GPIO as gpio
 gpio.setmode(gpio.BOARD)
 shift_register = pi74HC595()
 
-
-shift_register = pi74HC595(35, 21, 20)
-shift_register.set_by_list([0, 1, 0, 1, 1, 1, 0, 0])
+def __init__(
+        DS: int = 11, # gpio.BOARD
+        ST: int = 13,
+        SH: int = 15,
+        daisy_chain: int = 1,
+    ):
+    shift_register = pi74HC595(35, 21, 20)
+    shift_register.set_by_list([0, 1, 0, 1, 1, 1, 0, 0])
